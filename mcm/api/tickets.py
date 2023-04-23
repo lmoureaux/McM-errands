4259690cb75d76946d9d86995bc25a389e1de0e6
Louis Moureaux <louis.moureaux@cern.ch>
2023-04-23 17:45:54 +0200

    Implement /tickets/{prepId}
    Allow only a single ticket to be retrieved at a time. Will add a bulk API later
    if needed.

--- mcm/api/tickets.py
+++ mcm/api/tickets.py
@@ -1,8 +1,17 @@
 # SPDX-FileCopyrightText: Louis Moureaux <louis.moureaux@cern.ch>
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+import itertools as it
+
 from connexion.problem import problem
+from pycouchdb.exceptions import NotFound
+
+from ..db import get_db
 
 
-def get(prepIds):
-    return problem(501, "Not Implemented", "This API endpoint is not implemented")
+def get(prepId):
+    db = get_db().database("mccms")
+    try:
+        return db.get(prepId)
+    except NotFound:
+        return problem(404, "Not Found", f"Ticket {prepId} does not exist")
